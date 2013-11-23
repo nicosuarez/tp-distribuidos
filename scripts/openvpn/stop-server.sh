02c22c21907ddbdfccdfed95db6ea6ed245a24ff
nicolas.suarez <nicolas.suarez@0730cec2-0114-8b77-3d29-ada04e72c99b>
2013-11-23 18:41:20 +0000

    Se modifico stop-server para que mate todo

--- scripts/openvpn/stop-server.sh
+++ scripts/openvpn/stop-server.sh
@@ -1,18 +1,32 @@
-echo Dando de baja todas las intefaces...
-ifconfig tap0 down
-ifconfig tap1 down
-ifconfig tap2 down
-ifconfig tap3 down
-ifconfig tap4 down
-ifconfig tap5 down
-ifconfig tap6 down
-ifconfig tap7 down
-ifconfig tap8 down
-ifconfig tap9 down
+#!/bin/bash
 
-echo Fin...
+. ./ip_port.sh
+. ./functions.sh
+
+godMode
+
+echo "Dando de baja todas las intefaces..."
+stopServices
+removeProxy
+killTunnels
+cleanInterface
+cleanRoutes
+
+#ifconfig tap0 down
+#ifconfig tap1 down
+#ifconfig tap2 down
+#ifconfig tap3 down
+#ifconfig tap4 down
+#ifconfig tap5 down
+#ifconfig tap6 down
+#ifconfig tap7 down
+#ifconfig tap8 down
+#ifconfig tap9 down
+
 # Matar todos los procesos
-#echo Matando todos los procesos de openvpn...
+echo "Matando todos los procesos de openvpn..."
 pkill -f openvpn
+
+echo "Fin..."
 
 
