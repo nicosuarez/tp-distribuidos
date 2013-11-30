74b253a9b99bbc51ac3c7090a92575ca10b5b3c8
nicolas.suarez <nicolas.suarez@0730cec2-0114-8b77-3d29-ada04e72c99b>
2013-11-30 18:57:46 +0000

    Subo scripts de prueba

---
+++ scripts/openvpn/test_traceroute.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+echo honda01--------------------------------------------------- 1> salida
+echo "`traceroute -m10 10.24.3.129`" 1>> salida
+
+echo honda02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.130`" 1>> salida
+
+echo honda03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.131`" 1>> salida 
+
+echo honda04--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.132`" 1>> salida 
+
+echo honda05VRRP--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.133`" 1>> salida 
+
+echo hondadns2--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.134`" 1>> salida 
+
+echo hondaHostA--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.135`" 1>> salida 
+
+echo zanella01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.10.5.1`" 1>> salida 
+
+echo zanella02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.10.5.2`" 1>> salida 
+
+echo zanella03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.10.5.3`" 1>> salida 
+
+echo zanella04--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.10.5.4`" 1>> salida 
+
+echo zanella05VRRP--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.10.5.5`" 1>> salida 
+
+echo zanella06HOstC--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.10.5.6`" 1>> salida 
+
+echo ktm01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.1`" 1>> salida 
+
+echo ktm02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.2`" 1>> salida 
+
+echo azel01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.92.27.4`" 1>> salida
+
+echo azel02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.92.27.3`" 1>> salida
+
+echo azel03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.92.27.2`" 1>> salida
+
+echo azel4FTP--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.92.27.1`" 1>> salida
+
+echo aprilia01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 133.43.1.13`" 1>> salida 
+
+echo aprilia02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 133.43.1.5`" 1>> salida 
+
+echo aprili03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 133.43.1.9`" 1>> salida 
+
+echo bmw01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.92.27.33`" 1>> salida 
+
+echo bmw2--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.92.27.34`" 1>> salida
+
+echo ducati01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.5`" 1>> salida 
+
+echo ducati02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.6`" 1>> salida 
+
+echo harley01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 192.168.8.1`" 1>> salida 
+
+echo harley02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 192.168.8.2`" 1>> salida 
+
+echo harley03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 192.168.8.3`" 1>> salida 
+
+echo harley04VRRP--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 192.168.8.4`" 1>> salida 
+
+echo harleyDNS1--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 192.168.8.5`" 1>> salida 
+
+echo harley06APACHE--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 192.168.8.71`" 1>> salida 
+
+echo derbi01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.65`" 1>> salida 
+
+echo derbi02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.66`" 1>> salida 
+
+echo derbiVRRP--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.67`" 1>> salida 
+
+echo derbiHOSTB--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.68`" 1>> salida 
+
+echo motomel01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.130`" 1>> salida 
+
+echo motomel02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.131`" 1>> salida 
+
+echo motomnelTELNET2--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.129`" 1>> salida 
+
+echo gilera01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.1`" 1>> salida 
+
+echo gilera02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.2`" 1>> salida 
+
+echo gilera03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.3`" 1>> salida 
+
+echo gileraTELNET1---------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.65`" 1>> salida 
+
+echo gileraDNSROOT----------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.1.5`" 1>> salida 
+
+echo yamaha01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 172.43.0.65`" 1>> salida
+
+echo yamaha02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 172.43.0.66`" 1>> salida
+
+echo yamaha03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 172.43.0.67`" 1>> salida
+
+echo kawsaki01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.33`" 1>> salida 
+
+echo kawasaki02--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.34`" 1>> salida 
+
+echo kawasaki03--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.35`" 1>> salida 
+
+echo suzuki01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.9`" 1>> salida 
+
+echo vespa01--------------------------------------------------- 1>> salida
+echo "`traceroute -m10 10.24.3.17`" 1>> salida 
+
+exit 0
+
